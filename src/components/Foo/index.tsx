17d71bfa715d46833dc44d010e38e00cb55adef6
红 <wxh1220@gmail.com>
2023-10-24 16:45:59 +0800

    feat: 组件 less 测试 （不推荐） (#7)

--- src/components/Foo/index.tsx
+++ src/components/Foo/index.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import './style.less'
+import styles from  './style.module.less'
+
 
 export interface FooProps {
   slogan?: React.ReactNode;
@@ -11,6 +14,12 @@ function Foo(props: React.PropsWithChildren<FooProps>) {
       <div className="my-slogan">
         <p>魔法师正在进行最后的仪式，为您带来一项惊艳功能</p>
         <strong>TBD: The Brilliant Discovery!</strong>
+      </div>
+      <div className="foo-cls">
+        123
+      </div>
+      <div className={styles.moduleFoo}>
+        234
       </div>
       {children}
       {/* This is index.tsx} */}
