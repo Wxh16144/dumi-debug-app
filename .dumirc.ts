034cf49d01510bd48f016d4641a898d694357287
wuxh <wxh1220@gmail.com>
2023-10-24 17:05:34 +0800

    test: 测试我的插件集

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'dumi';
 const base = process.env.MY_SITE_BASE ?? '/website/';
 
 export default defineConfig({
+  plugins:[
+    'dumi-plugin-code-snippets',
+    'dumi-plugin-color-chunk'
+  ],
   locales: [
     { id: 'en-US', name: 'English' },
     { id: 'zh-CN', name: '中文' },
