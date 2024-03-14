0d990db2bf9dc131a56cdc04ef1d8e034f823749
红 <wxh1220@gmail.com>
2024-03-14 17:09:51 +0800

    test: 覆写三方主题测试 (#9)
    * feat(theme): 添加三方主题 dumi-theme-antd-style
    
    * chore: 添加注释
    
    * chore: 添加了三方主题包验证

--- .dumi/theme/slots/Footer/index.tsx
+++ .dumi/theme/slots/Footer/index.tsx
@@ -1,7 +1,7 @@
+// theme docs: https://d.umijs.org/theme
 import React from 'react'
 
-const App = () => {
-
+const LocalFooter = () => {
   return (
     <div style={{
       width: '100%',
@@ -15,6 +15,20 @@ const App = () => {
   )
 }
 
+// export default LocalFooter
+// export { default } from 'dumi/theme-default/slots/Footer' /** dumi builtin themes */
 
-// export default App
-export { default } from 'dumi/theme-default/slots/Footer'
+import Footer from 'dumi/theme-original/slots/Footer' /** three parties themes */
+export default () => (
+  <div>
+    <p style={{ fontSize: 24, color: 'blue' }}>下面是主题包的 Footer</p>
+    <Footer />
+  </div>
+)
+
+/**
+ * 总结
+ * dumi/theme-original/{layouts,builtins,slots}/xx 为三方主题包
+ * dumi/theme-default/{layouts,builtins,slots}/xx 为 dumi 内置主题包
+ * dumi/theme/{layouts,builtins,slots}/xx 为当前项目 .dumi/theme/xx 里的文件主题
+ */
